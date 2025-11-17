bc5d6287e7b9d922175e1514ac3cd8968a3be7d9
Sidy El Moctar Dione <sidyelmoctar.dione@ucad.edu.sn>
2025-11-17 16:27:44 +0000

    premier commit

---
+++ PrevisionBudgetaire/src/main/java/com/previsionbudgetaire/PrevisionBudgetaireApplication.java
@@ -0,0 +1,13 @@
+package com.previsionbudgetaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisionBudgetaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrevisionBudgetaireApplication.class, args);
+    }
+
+}
---
+++ PrevisionBudgetaire/src/test/java/com/previsionbudgetaire/PrevisionBudgetaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previsionbudgetaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisionBudgetaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
